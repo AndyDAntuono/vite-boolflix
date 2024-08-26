1b73f505e29358db92f71ddb80d66fa85dffc323
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-08-26 19:28:15 +0200

    [UPD] upadated App.vue and AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <header>
+            <h1>Boolfix</h1>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
