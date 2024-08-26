ac70a911fa06dcd8682ad0069412d365b0914e27
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-08-26 20:43:36 +0200

    [UPD] updated AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,21 @@
 <template>
-    <div class="container">
+    <div class="container bg-dark py-2">
         <header>
-            <h1>Boolfix</h1>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <div class="row">
+                <div class="col-4">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <h1 class="rounded bg-danger text-white p-1">Boolfix</h1>
+                    </div>
+                </div>
+                <div class="col-8">
+                    <div>
+                        <form class="form-inline my-2 my-lg-0">
+                            <input class="d-inline form-control mr-sm-2 w-75" type="search" placeholder="Search" aria-label="Search">
+                            <button class="d-inline btn btn-outline-success ms-2 my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </header>
     </div>
 </template>
