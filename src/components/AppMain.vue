2d83088e3d2edd93cebd3ca6ff5eface95f9878e
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-08-27 09:53:58 +0200

    [UPD] updated AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,19 @@
 <template>
     <main>
         <div class="container">
-            <div class="card">
-                <img src="" alt="film-img">
-                <div class="card-body">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Titolo film</li>
-                        <li class="list-group-item">Titolo originale</li>
-                        <li class="list-group-item">Lingua (inserire qui bandiera)</li>
-                        <li class="list-group-item">Voto</li>
-                      </ul>
+            <div class="row">
+                <div class="col-3">
+                    <div class="card">
+                        <img src="" alt="film-img">
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Titolo film</li>
+                                <li class="list-group-item">Titolo originale</li>
+                                <li class="list-group-item">Lingua (inserire qui bandiera)</li>
+                                <li class="list-group-item">Voto</li>
+                              </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
